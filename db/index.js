010bfdbbd7709a323dec5e532a4cc0e854da9d84
kwegorek <ka.wegorek@gmail.com>
2020-03-22 00:38:15 +0100

    Config db

--- db/index.js
+++ db/index.js
@@ -6,20 +6,13 @@ const {
 // Option 1: Passing a connection URI
 // https://sequelize.org/master/manual/getting-started.html
 
-const sequelize = new Sequelize('postgres://localhost:5432/yourdbname', {
-  logging: console.log
+const sequelize = new Sequelize('postgres://localhost:5432/template_db', {
+  logging: false
 }) // Example for postgres
 
 const User = sequelize.define("user", {
   name: DataTypes.TEXT,
 });
 
-(async () => {
-  await sequelize.sync({
-    alter: true
-  });
+module.exports = sequelize;
 
-  // Code here
-})();
-
-//alter: true; can be destructive operations. Therefore, they are not recommended for production-level software
--- index.js
+++ index.js
@@ -1,48 +1,52 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const PORT = 8000
-
+const sequelize = require('./db/index.js')
+const PORT = process.env.PORT || 8000
 
 //--------------------->Body parser <---------------------//
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+)
 
 //--------------------->Loggin middleware morgan https://github.com/expressjs/morgan  <---------------------//
-const morgan = require('morgan');
-app.use(morgan('dev'));
+const morgan = require('morgan')
+app.use(morgan('dev'))
 
 //--------------------->Serving javascript files, css files, and images from public folder<---------------------//
 //https://expressjs.com/en/starter/static-files.html
 
-
 // static file-serving middleware
 app.use(express.static(path.join(__dirname, '.', 'public')))
 
-app.get('*', function (req, res, next) {
+app.get('*', function(req, res, next) {
   res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
-// API routes are prefixed with /api/ - 
+// API routes are prefixed with /api/ -
 // this is purely done to namespace them away from your "front-end routes" (such as those created by react-router).
-app.use('/api', require('./apiRoutes')); // matches all requests to /api
+app.use('/api', require('./apiRoutes')) // matches all requests to /api
 
-
-app.use(function (err, req, res, next) {
-  console.error(err);
-  console.error(err.stack);
-  res.status(err.status || 500).send(err.message || 'Internal server error.');
-});
+app.use(function(err, req, res, next) {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
 
 //--------------------->Starting server<--------------------- //
-// process.env.PORT  
+
 const startServer = () => {
   const server = app.listen(process.env.PORT || PORT, () =>
     console.log(`Listening on ${PORT}`)
   )
 }
 
-startServer()
+;(async () => {
+  await sequelize.sync()
+
+  console.log('hello')
+})(startServer())
