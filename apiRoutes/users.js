ea1a712e2f93ee920566981cd309b2cce4cd2bdb
kwegorek <50807299+kwegorek@users.noreply.github.com>
2020-03-29 18:55:06 +0200

    updating users.js

--- apiRoutes/users.js
+++ apiRoutes/users.js
@@ -1,14 +1,14 @@
-// apiRoutes/puppies.js
+// apiRoutes/users.js
 const router = require('express').Router();
 // Express supports methods that correspond to all HTTP request methods: get, post, and so on
 
-// matches GET requests to /api/puppies/
+// matches GET requests to /api/users/
 router.get('/', function (req, res, next) { /* etc */});
-// matches POST requests to /api/puppies/
+// matches POST requests to /api/users/
 router.post('/', function (req, res, next) { /* etc */});
-// matches PUT requests to /api/puppies/:puppyId
+// matches PUT requests to /api/users/:usersId
 router.put('/:userId', function (req, res, next) { /* etc */});
-// matches DELETE requests to /api/puppies/:puppyId
+// matches DELETE requests to /api/users/:usersId
 router.delete('/:userId', function (req, res, next) { /* etc */});
 
 module.exports = router;
