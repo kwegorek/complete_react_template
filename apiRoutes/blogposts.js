781cc1a4d175555fe4013bdd90cdb61fb85445ca
kwegorek <50807299+kwegorek@users.noreply.github.com>
2020-03-29 18:56:14 +0200

    updating blogposts

--- apiRoutes/blogposts.js
+++ apiRoutes/blogposts.js
@@ -1,14 +1,14 @@
-// apiRoutes/puppies.js
+// apiRoutes/blogposts.js
 const router = require('express').Router();
 // Express supports methods that correspond to all HTTP request methods: get, post, and so on
 
-// matches GET requests to /api/puppies/
+// matches GET requests to /api/blogposts/
 router.get('/', function (req, res, next) { /* etc */});
-// matches POST requests to /api/puppies/
+// matches POST requests to /api/blogposts/
 router.post('/', function (req, res, next) { /* etc */});
-// matches PUT requests to /api/puppies/:puppyId
+// matches PUT requests to /api/blogpostss/:blogpostsId
 router.put('/:blogpostId', function (req, res, next) { /* etc */});
-// matches DELETE requests to /api/puppies/:puppyId
+// matches DELETE requests to /api/blogposts/:blogpostsId
 router.delete('/:blogpostId', function (req, res, next) { /* etc */});
 
 module.exports = router;
